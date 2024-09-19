60e4ec20355dcea9484083e38c2bb1ccbb57d627
Karl <karl@ltek.me>
2024-09-19 17:17:24 +0800

    working chat bot

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,28 +1,54 @@
 import { useEffect, useState } from "react";
 import { getChatId } from "../components/settings";
-import { getChatLogs } from "../components/api/actions";
-import { StatusLoading } from "../components/api/status";
+import { getChatLogs, queryApi } from "../components/api/actions";
+import { StatusLoading, StatusThinking } from "../components/api/status";
 
 import '../styles/chat.css'
-import { json } from "stream/consumers";
 
-interface ChatLog {
+import { ReactNode } from "react";
+
+interface ChatLogItems {
     timestamp: number,
     role: string,
-    text: string
+    text: string | ReactNode
 }
 
 export const Chat = () => {
 
     const chatId = getChatId();
-    const [chatLog, setChatLog] = useState<ChatLog[]>([]);
+    const [chatLog, setChatLog] = useState<ChatLogItems[]>([]);
     const [waitingApi, setWaitingApi] = useState(true);
-    const [waitingNewMessage, setWaitingNewMessage] = useState(false);
     const [message, setMessage] = useState('');
 
-    const handleSendMessage = () => {
-
+    const handleSendMessage = async () => {
+        const initMsgTime = Math.floor(new Date().getTime() / 1000)
+        let newChatLog = [...chatLog, {
+            timestamp: initMsgTime,
+            role: 'human',
+            text: message,
+        }, {
+            timestamp: initMsgTime,
+            role: 'assistant',
+            text: <StatusThinking />,
+        }]
+        setChatLog(newChatLog);
+        sendApiRequest(message, newChatLog);
+        setMessage('');
+        localStorage.removeItem(`chat_logs_${chatId}`);
     }
+
+    const sendApiRequest = async (msg: string, newChatLog: ChatLogItems[]) => {
+        const apiResponse = await queryApi('', msg, chatId);
+        const updatedChatLog = newChatLog.map((item, index) => {
+            if (index === newChatLog.length - 1) {
+                return { ...item, text: apiResponse.message };
+            }
+            return item;
+        });
+        setChatLog(updatedChatLog);
+        localStorage.setItem(`chat_logs_${chatId}`, JSON.stringify(updatedChatLog));
+    }
+
 
     useEffect(() => {
         const funcBoo = async () => {
@@ -30,6 +56,7 @@ export const Chat = () => {
             try {
                 if (cachedChatLog) {
                     setChatLog(JSON.parse(cachedChatLog));
+                    setWaitingApi(false);
                 } else {
                     let logs = await getChatLogs({ chatId: chatId });
                     setChatLog(logs);
@@ -38,7 +65,7 @@ export const Chat = () => {
                 }
             } catch (error) {
                 console.error(error);
-                console.warn('Dumping Cached Chat Logs');
+                console.warn('Chat: Dumping Cached Chat Logs');
                 localStorage.removeItem(`chat_logs_${chatId}`);
                 let logs = await getChatLogs({ chatId: chatId });
                 setChatLog(logs);
@@ -60,8 +87,8 @@ export const Chat = () => {
                             {chatLog.map((r, index) => (
                                 <tr key={index}>
                                     <td>{new Date(r.timestamp * 1000).toLocaleString()}</td>
-                                    <td>{r.role}</td>
-                                    <td>{r.text}</td>
+                                    <td>{r.role === 'assistant' ? 'meow' : 'human'}</td>
+                                    <td>{typeof r.text === 'string' ? <span dangerouslySetInnerHTML={{ __html: r.text }}></span> : r.text}</td>
                                 </tr>
                             ))}
                         </tbody>
@@ -70,6 +97,7 @@ export const Chat = () => {
                         <textarea
                             name="message"
                             placeholder="Message to sent"
+                            value={message}
                             onChange={(e) => { setMessage(e.target.value) }}
                             rows={5}
                             cols={70}
